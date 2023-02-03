117e42cbe7ad3bf07edc741afa63715ea143c28b
jerry pierre <115857393+jpierre9025@users.noreply.github.com>
2023-02-03 15:46:19 -0500

    updated index file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function demo (name, height, country) {
-    alert ('name' + "  " + 'height' + "  " +'country')
+function demo(name, height, country) {
+    alert(name + "  " + height + "  " + country)
 }
 demo("Jay", "6'1", "USA")
